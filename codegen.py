7ea53db2b7c49164fa72f1322e35806cd147367a
EllenKar <ellenk@windowslive.com>
2023-02-06 10:02:20 +0000

    first commit

---
+++ codegen.py
@@ -0,0 +1,22 @@
+print("----------------------------")
+print("CODE GENERATOR")
+print("----------------------------")
+print("Make your password more secure with encryption!")
+print()
+encrypt_cod = input("Please put down a text password, no caps.")
+
+def encryptious(encrypt_cod):
+    encrypted = encrypt_cod.maketrans("abcdefghijklmnopqrstuvwxyz", "wvutsrqponmlkjihgfedcbazyx")
+    crypt_ed = encrypt_cod.translate(encrypted)
+    return crypt_ed
+
+print(encryptious(encrypt_cod))
+print()
+decrypt_cod = input("Please input your encrypted password to decrypt it.")
+
+def decryptious(decrypt_cod):
+    decrypted = decrypt_cod.maketrans("wvutsrqponmlkjihgfedcbazyx", "abcdefghijklmnopqrstuvwxyz")
+    crypt_de = decrypt_cod.translate(decrypted)
+    return crypt_de
+
+print(encryptious(decrypt_cod))
