40d84cfce79eb131ba4133bde8ed7eeabc02332d
EllenKar <ellenk@windowslive.com>
2023-02-25 10:36:34 +0000

    added comments

--- codegen.py
+++ codegen.py
@@ -4,7 +4,7 @@ print("----------------------------")
 print("Make your password more secure with encryption!")
 print()
 encrypt_cod = input("Please put down a text password, no caps.")
-
+#the below creates two variables that substitute a letter for another, and back.
 def encryptious(encrypt_cod):
     encrypted = encrypt_cod.maketrans("abcdefghijklmnopqrstuvwxyz", "wvutsrqponmlkjihgfedcbazyx")
     crypt_ed = encrypt_cod.translate(encrypted)
@@ -13,7 +13,7 @@ def encryptious(encrypt_cod):
 print(encryptious(encrypt_cod))
 print()
 decrypt_cod = input("Please input your encrypted password to decrypt it.")
-
+#the below does the opposite, to decrypt the encrypted.
 def decryptious(decrypt_cod):
     decrypted = decrypt_cod.maketrans("wvutsrqponmlkjihgfedcbazyx", "abcdefghijklmnopqrstuvwxyz")
     crypt_de = decrypt_cod.translate(decrypted)
